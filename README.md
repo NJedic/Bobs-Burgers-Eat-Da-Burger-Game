cf98f14c7a0efe5b8815effbd885cc568b2d2758
Nikki Jedic <nikki.jedic@gmail.com>
2017-07-17 19:57:56 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Burger
+# Bob's Burgers Eat-Da-Burger Game (in progress)
+
+![Work in Progress](https://media.giphy.com/media/bFTSLXI4Of15u/giphy.gif)
---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+	id INTEGER(100) AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(100) NOT NULL,
+	devoured BOOLEAN NOT NULL,
+	date TIMESTAMP,
+	PRIMARY KEY id
+);
+
+SELECT * FROM burgers;
